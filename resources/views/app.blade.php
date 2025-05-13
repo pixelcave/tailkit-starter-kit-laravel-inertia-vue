92d044cd3856ddcbc4c03c9fb8e7f45392db64c5
John Champ <72492771+john-champ@users.noreply.github.com>
2025-05-13 13:17:32 +0300

    Added class="antialiased"

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -21,7 +21,7 @@
     @vite(["resources/js/app.js", "resources/js/Pages/{$page["component"]}.vue"])
     @inertiaHead
   </head>
-  <body>
+  <body class="antialiased">
     @inertia
   </body>
 </html>
